84989a82723e946910f5c328b993509f2d248728
Romain <romain.carlier2@gmail.com>
2023-10-17 10:24:46 +0200

    continue findthebug exercice

--- ScriptBash/buggy.sh
+++ ScriptBash/buggy.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
- n=$1
+n=$1
 
- if [[ n -eq 42 ]]; then
-    echo "Quelque chose s'est mal passé"
-    >&2 echo "L'erreur provient d'un nombre magique !"
-    exit 1
- fi
+if [[ $n -eq 42 ]]; then
+exit 1
+fi
 
- echo "Tout s'est bien passé"
+exit 0
